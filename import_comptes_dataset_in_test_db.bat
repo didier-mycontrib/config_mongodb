db83aeaf7347de7478e335aa620d80ffcbd6a28d
didier <didier@d-defrance.fr>
2020-05-27 18:34:31 +0200

    2020 v4.2

--- import_comptes_dataset_in_test_db.bat
+++ import_comptes_dataset_in_test_db.bat
@@ -1,4 +1,4 @@
-set MONGO_HOME=C:\Program Files\MongoDB\Server\3.2
+set MONGO_HOME=C:\Program Files\MongoDB\Server\3.4
 cd /d "%~dp0"
 "%MONGO_HOME%\bin\mongoimport" --db test --collection comptes --drop --file dataset/comptes.json --jsonArray
 "%MONGO_HOME%\bin\mongoimport" --db test --collection clients --drop --file dataset/clients.json --jsonArray
--- install_MongoDB_service_as_admin.bat
+++ install_MongoDB_service_as_admin.bat
@@ -1,3 +1,3 @@
-set MONGO_HOME=C:\Program Files\MongoDB\Server\3.2
+set MONGO_HOME=C:\Program Files\MongoDB\Server\3.4
 "%MONGO_HOME%\bin\mongod.exe" --config "C:\Prog\MongoDB\mongodb.cfg" --install
 pause
